081d2f7fb8d3bc77f40802a11fadae7f9f2c858b
delishadalu <73183936+delishadalu@users.noreply.github.com>
2022-08-20 19:56:18 +0530

    Add files via upload

--- c127.py
+++ c127.py
@@ -4,3 +4,35 @@ from bs4 import BeautifulSoup
 import time
 import pandas as pd
 
+
+
+
+planets_data=[]
+
+def scrape():
+    for i in range(0, 10):
+        print(f'Scrapping page {i+1} ...')
+
+        # BeautifulSoup Object
+        soup = BeautifulSoup(browser.page_source, "html.parser")
+
+        # Loop to find element using XPATH
+        for ul_tag in soup.find_all("ul", attrs={"class", "exoplanet"}):
+            li_tags = ul_tag.find_all("li")
+
+
+
+            temp_list = []
+
+            for index, li_tag in enumerate(li_tags):
+
+                if index == 0:
+                    temp_list.append(li_tag.find_all("a")[0].contents[0])
+                else: 
+                    temp_list.append(li_tag.contents[0])
+                    
+
+            planets_data.append(temp_list)
+
+        # Find all elements on the page and click to move to the next page
+        browser.find_element(by=By.XPATH, value='//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
